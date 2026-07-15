08780333a5751b142e81e543fe08d368b52bcaae
Eduardo Urias <carlos2021.urias@gmail.com>
2026-07-15 16:36:22 -0600

    Avance
    Ayuda

--- Base de datos/index.sql
+++ Base de datos/index.sql
@@ -1,10 +1,10 @@
-CREATE DATABASE amonos;
+CREATE DATABASE IF NOT EXISTS amonos;
  
 USE amonos;
  
-CREATE TABLE usuarios(
+CREATE TABLE IF NOT EXISTS usuarios(
 	id_usuario INT PRIMARY KEY AUTO_INCREMENT,
-	nombre VARCHAR(100), NOT NULL
+	nombre VARCHAR(100) NOT NULL,
 	correo VARCHAR(200), 
 	password_hash VARCHAR(255),
 	estado ENUM(
@@ -13,50 +13,14 @@ CREATE TABLE usuarios(
 	)DEFAULT 'activo'
 );
  
-CREATE TABLE resenas(
+CREATE TABLE IF NOT EXISTS resenas(
 	id_resena INT PRIMARY KEY AUTO_INCREMENT,
 	id_usuario INT NOT NULL,
 	puntuacion INT,
 	comentario TEXT,
-	fecha DATETIME DEFAULT CURRENT_TIMESTAMP
-);
- 
- 
-CREATE TABLE favoritos(
-	id_favorito INT AUTO_INCREMENT PRIMARY KEY,
-	id_usuario INT NOT NULL,
-	id_destino INT NOT NULL,
-	fecha_agregado DATETIME DEFAULT CURRENT_TIMESTAMP
- 
-);
- 
-CREATE TABLE historial_visitas(
-	id_visita INT AUTO_INCREMENT PRIMARY KEY,
-	id_usuario INT NOT NULL,
-	id_destino INT NOT NULL,
-	fecha_visita DATE NOT NULL,
-	calificacion TINYINT NULL,
-	gasto_total DECIMAL(10,2)
-);
- 
-CREATE TABLE presupuestos_viaje(
-	id_presupuesto INT AUTO_INCREMENT PRIMARY KEY,
-	id_usuario INT NOT NULL,
-	id_destino INT NOT NULL,
-	nombre_presupuesto VARCHAR(100) NULL,
-	cantidad_personas INT NOT NULL DEFAULT 1,
-	incluir_entrada BOOLEAN DEFAULT TRUE,
-	incluir_comida BOOLEAN DEFAULT TRUE,
-	incluir_parqueo BOOLEAN DEFAULT FALSE,
-	incluir_hospedaje BOOLEAN DEFAULT FALSE,
-	costo_entrada DECIMAL(10,2) DEFAULT 0,
-	costo_comida DECIMAL(10,2) DEFAULT 0,
-	costo_parqueo DECIMAL(10,2) DEFAULT 0,
-	costo_hospedaje DECIMAL(10,2) DEFAULT 0,
-	presupuesto_usuario DECIMAL(10,2) NOT NULL,
-	costo_estimado DECIMAL(10,2) NOT NULL,
-	dinero_restante DECIMAL(10,2) NOT NULL,
-	fecha_calculo DATETIME DEFAULT CURRENT_TIMESTAMP
+	fecha DATETIME DEFAULT CURRENT_TIMESTAMP,
+	FOREIGN KEY(id_usuario)
+	REFERENCES usuarios(id_usuario)
 );
 
 CREATE TABLE destinos(
@@ -77,8 +41,60 @@ CREATE TABLE destinos(
 	'jovenes',
 	'pareja',
 	'todos'
-	)DEFAULT 'todos'
+	)DEFAULT 'todos',
+	
 );
+ 
+CREATE TABLE favoritos(
+	id_favorito INT AUTO_INCREMENT PRIMARY KEY,
+	id_usuario INT NOT NULL,
+	id_destino INT NOT NULL,
+	fecha_agregado DATETIME DEFAULT CURRENT_TIMESTAMP,
+	FOREIGN KEY(id_usuario)
+	REFERENCES usuarios(id_usuario),
+	FOREIGN KEY(id_destino)
+	REFERENCES destinos(id_destinos)
+ 
+);
+ 
+CREATE TABLE historial_visitas(
+	id_visita INT AUTO_INCREMENT PRIMARY KEY,
+	id_usuario INT NOT NULL,
+	id_destino INT NOT NULL,
+	fecha_visita DATE NOT NULL,
+	calificacion TINYINT NULL,
+	gasto_total DECIMAL(10,2),
+	FOREIGN KEY(id_usuario)
+	REFERENCES usuarios(id_usuario),
+	FOREIGN KEY(id_destino)
+	REFERENCES destinos(id_destino)
+);
+ 
+CREATE TABLE presupuestos_viaje(
+	id_presupuesto INT AUTO_INCREMENT PRIMARY KEY,
+	id_usuario INT NOT NULL,
+	id_destino INT NOT NULL,
+	nombre_presupuesto VARCHAR(100) NULL,
+	cantidad_personas INT NOT NULL DEFAULT 1,
+	incluir_entrada BOOLEAN DEFAULT TRUE,
+	incluir_comida BOOLEAN DEFAULT TRUE,
+	incluir_parqueo BOOLEAN DEFAULT FALSE,
+	incluir_hospedaje BOOLEAN DEFAULT FALSE,
+	costo_entrada DECIMAL(10,2) DEFAULT 0,
+	costo_comida DECIMAL(10,2) DEFAULT 0,
+	costo_parqueo DECIMAL(10,2) DEFAULT 0,
+	costo_hospedaje DECIMAL(10,2) DEFAULT 0,
+	presupuesto_usuario DECIMAL(10,2) NOT NULL,
+	costo_estimado DECIMAL(10,2) NOT NULL,
+	dinero_restante DECIMAL(10,2) NOT NULL,
+	fecha_calculo DATETIME DEFAULT CURRENT_TIMESTAMP,
+	FOREIGN KEY(id_usuario)
+	REFERENCES usuarios(id_usuario),
+	FOREIGN KEY(id_destino)
+	REFERENCES destinos(id_destino)
+);
+
+
 
 CREATE TABLE categorias_destino(
 	id_categoria INT AUTO_INCREMENT PRIMARY KEY,
@@ -89,7 +105,9 @@ CREATE TABLE categorias_destino(
 CREATE TABLE imagenes_destino(
 	id_imagen INT AUTO_INCREMENT PRIMARY KEY,
 	id_destino INT NOT NULL,
-	imagen_url VARCHAR(255)
+	imagen_url VARCHAR(255),
+	FOREIGN KEY(id_destino)
+	REFERENCES destinos(id_destino)
 );
 
 CREATE TABLE actividades(
@@ -101,8 +119,8 @@ CREATE TABLE actividades(
 	'jet ski',
 	'nadar',
 	'paseo en lancha',
-	'restaurante'
+	'restaurante',
+	FOREIGN KEY(id_destino)
+	REFERENCES destinos(id_destino)
+	)
 );
-
-
-
